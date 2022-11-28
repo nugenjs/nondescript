66091a7437f7bd568f4cd63f71bd3def027ffd64
John <jn183116@gmail.com>
2022-11-28 17:45:41 -0600

    let to const, separate input params

--- screenCurve.js
+++ screenCurve.js
@@ -1,30 +1,31 @@
-let radius = 3800; // in mm
-let screenWidthIn = 31.25;
+const radius = 3800; // in mm
+const screenWidthIn = 31.25;
+// ~~~~~~~~~~~~~~~~~~~~~~~~
 
 
-let screenMM = screenWidthIn * 25.4; 
+const screenMM = screenWidthIn * 25.4; 
 
 // Circumference
-let circ = 2 * Math.PI * radius;
+const circ = 2 * Math.PI * radius;
 
 // Angle from screen width
 // L/360 = screenMM/circ
 // L = 360 * 
-let angle = 360 * screenMM / circ;
+const angle = 360 * screenMM / circ;
 
 console.log(`angle = ${angle}`)
 console.log(11.968*circ/360)
 console.log(screenMM)
 
 // x=r∗sin(θ),y=r∗cos(θ)
-let x0 = radius * Math.sin(0);
-let y0 = radius * Math.cos(0);
+const x0 = radius * Math.sin(0);
+const y0 = radius * Math.cos(0);
 
-let xH = radius * Math.sin(angle / 2);
-let yH = radius * Math.cos(angle / 2);
+const xH = radius * Math.sin(angle / 2);
+const yH = radius * Math.cos(angle / 2);
 
-let xL = radius * Math.sin(angle);
-let yL = radius * Math.cos(angle);
+const xL = radius * Math.sin(angle);
+const yL = radius * Math.cos(angle);
 
 console.log(`x0 = ${x0}`);
 console.log(`y0 = ${y0}`);
@@ -36,11 +37,11 @@ console.log(`xL = ${xL}`);
 console.log(`yL = ${yL}`);
 
 
-let xMid = (x0 + xL) / 2
-let yMid = (y0 + yL) / 2
+const xMid = (x0 + xL) / 2
+const yMid = (y0 + yL) / 2
 
-let xDistToH = Math.abs(xH - xMid);
-let yDistToH = Math.abs(yH - yMid);
+const xDistToH = Math.abs(xH - xMid);
+const yDistToH = Math.abs(yH - yMid);
 
 console.log(`xMid = ${xMid}`)
 console.log(`yMid = ${yMid}`)
@@ -49,7 +50,7 @@ console.log(`xDistToH = ${xDistToH}`)
 console.log(`yDistToH = ${yDistToH}`)
 
 
-let dist = Math.sqrt(xDistToH*xDistToH + yDistToH*yDistToH)
+const dist = Math.sqrt(xDistToH*xDistToH + yDistToH*yDistToH)
 
 
 console.log(`dist = ${dist}`)
--- screenSize.js
+++ screenSize.js
@@ -1,6 +1,6 @@
-let ratio = 21/9;
-let c = 49;
-let a, b;
+const ratio = 21/9;
+const c = 49;
+// ~~~~~~~~~~~~~~~~~~~~~~~~
 
 // aa + bb = cc;
 // #21/9 = a/b
@@ -9,8 +9,8 @@ let a, b;
 // bb = cc/(21/9 + 1)
 // b = sqrt((21/9 + 1)cc)
 // a = 21/9b
-b = Math.sqrt(c*c / (ratio*ratio + 1));
-a = ratio * b;
+const b = Math.sqrt(c*c / (ratio*ratio + 1));
+const a = ratio * b;
 console.log(`a = ${a.toFixed(2)}in, b = ${b.toFixed(2)}in`)
 // console.log(`${a*a} + ${b*b} = ${c*c}`) // CHECK
 // console.log(`c^2 - a^2 = ${c*c - a*a}`) // CHECK
